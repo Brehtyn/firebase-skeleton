3e720baf209ec02a27b103de7f18bf7bf74b1c6d
Brehtyn <66739729+Brehtyn@users.noreply.github.com>
2022-02-03 12:27:34 -0800

    vue migration
    too much too list, following compositional api ch 9 of
    
    https://www.udemy.com/course/build-web-apps-with-vuejs-firebase/learn/lecture/23585370#overview

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    Viewer
+    <p> My name and age are {{name}} & {{age}}.</p>
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Home',
+  setup() {
+    console.log('Home setup')
+
+    const name = 'mario'
+    const age = 30
+
+    const handleClick = () => {
+      console.log('clicked')
+    }
+
+    return { name, age, handleClick }
+  }
+}
+</script>
